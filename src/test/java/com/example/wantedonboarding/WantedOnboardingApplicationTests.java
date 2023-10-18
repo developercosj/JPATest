9d84a72a9eb4f1bc050721f94811d6edc57e54e0
sujeonglee <developercosj@gmail.com>
2023-10-18 17:28:55 +0900

    [이수정] Test 케이스 작성 (유닛테스트) - 유저생성 - 회사생성 등의 DB 연결 테스트와 JPA 사용 테스트

---
+++ src/test/java/com/example/wantedonboarding/WantedOnboardingApplicationTests.java
@@ -0,0 +1,90 @@
+package com.example.wantedonboarding;
+
+import com.example.wantedonboarding.interfaceFile.RepoCompany;
+import com.example.wantedonboarding.interfaceFile.RepoHistory;
+import com.example.wantedonboarding.interfaceFile.RepoPosting;
+import com.example.wantedonboarding.interfaceFile.RepoUser;
+import com.example.wantedonboarding.model.Company;
+import com.example.wantedonboarding.model.History;
+import com.example.wantedonboarding.model.Posting;
+import com.example.wantedonboarding.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+class WantedOnboardingApplicationTests {
+
+    @Autowired
+    RepoPosting repoPosting;
+    @Autowired
+    RepoCompany repoCompany;
+    @Autowired
+    RepoHistory repoHistory;
+    @Autowired
+    RepoUser repoUser;
+
+
+    // 데이터 등록 테스트
+    //@BeforeEach
+    @Test
+    void insertTestData() {
+        Posting posting = new Posting();
+        //posting.setPostingIdx(1);
+        posting.setCompanyName("테스트회사이름1");
+        posting.setCity("테스트시트1");
+        repoPosting.save(posting);
+
+        posting = new Posting();
+        posting.setCompanyName("테스트회사이름2");
+        repoPosting.save(posting);
+
+        posting = new Posting();
+        posting.setCompanyName("테스트회사이름3");
+        repoPosting.save(posting);
+
+        posting = new Posting();
+        posting.setCompanyName("테스트회사이름4");
+        repoPosting.save(posting);
+
+        posting = new Posting();
+        posting.setCompanyName("테스트회사이름5");
+        repoPosting.save(posting);
+
+    }
+
+
+    // 회사 생성 테스트
+    @Test
+    void insertCompanyDate() {
+        Company company = new Company();
+        company.setCompanyId("wanted1");
+        company.setCompanyName("원티드1");
+        repoCompany.save(company);
+    }
+
+    // 유저 생성 테스트
+    @Test
+    void insertUserDate() {
+        User user = new User();
+        user.setUserId("intern1");
+        user.setUserName("인턴1");
+        repoUser.save(user);
+    }
+
+
+    @Test
+    void contextLoads() {
+        History history = new History();
+        history.setUserId("0");
+        history.setCompanyId("testpr");
+        repoHistory.save(history);
+    }
+
+
+
+
+
+}
