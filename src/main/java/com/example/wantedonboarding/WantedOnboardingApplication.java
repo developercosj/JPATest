4c036ea6c882a15d3beddb8768e771b6a51dc1a7
sujeonglee <developercosj@gmail.com>
2023-10-18 17:29:55 +0900

    [이수정] main 클래스

---
+++ src/main/java/com/example/wantedonboarding/WantedOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.example.wantedonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedOnboardingApplication.class, args);
+    }
+
+}
